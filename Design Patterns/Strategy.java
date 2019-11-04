0ff9e85dba0b9fca6d4b2cf43ea286463342ccfd
Romain DALICHAMP <romain.dalichamp@free.fr>
2019-11-03 20:37:44 -0500

    Update Strategy.java

--- Design Patterns/Strategy.java
+++ Design Patterns/Strategy.java
@@ -7,7 +7,11 @@
 public interface Strategy {
    public int doOperation(int num1, int num2);
 }
-//Implementations
+/**
+ * ##########################################################################
+ * ######################## implementations #################################
+ * ##########################################################################
+ */
 public class OperationMultiply implements Strategy{
    @Override
    public int doOperation(int num1, int num2) {
